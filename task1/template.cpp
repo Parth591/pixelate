0e0faffb92d49be7eedac2069a566d7909c2e9f8
Parth591 <gadaparth591@gmail.com>
2026-04-05 15:29:37 +0530

    Task1_Solution

--- task1/template.cpp
+++ task1/template.cpp
@@ -1,5 +1,7 @@
 #include <iostream>
 #include <opencv2/opencv.hpp>
+#include <convolution.hpp>
+#include <bits/stdc++.h>
 
 using namespace std;
 
@@ -18,14 +20,33 @@ int main()
          1,  0, -1
     );
 
-    cv::Mat img = cv::imread("./assets/hogwarts.png", cv::IMREAD_GRAYSCALE);
+    cv::Mat img = cv::imread("./assets/hogwarts.png", cv::IMREAD_COLOR);
+    cv::Mat img_f;
+    img.convertTo(img_f, CV_64FC1);
+
     if (img.empty()) {
         cerr << "Could not load image at ./assets/hogwarts.png\n";
         return -1;
     }
 
-    cv::Mat img_f;
-    img.convertTo(img_f, CV_64FC1);
+    cv::Mat K1_v = (cv::Mat_<double>(3, 1) << 1, 0, -1);
+    cv::Mat K1_h = (cv::Mat_<double>(1, 3) << 1, 2, 1);
+
+    cv::Mat K2_v = (cv::Mat_<double>(3, 1) << 1, 2, 1);
+    cv::Mat K2_h = (cv::Mat_<double>(1, 3) << 1, 0, -1);
+
+    cv::Mat intermediate1;
+    cv::Mat intermediate2;
+    cv::Mat intermediate3;
+
+    intermediate1 = convolve(img, K1_v);         // Convolve Vertically
+    intermediate2 = convolve(intermediate1, K1_h);        // Convolve Horizontally
+    intermediate3 = convolve(intermediate2, K2_v);
+    img_f = convolve(intermediate3, K2_h);
+
+    cv::namedWindow("Output", cv::WINDOW_NORMAL);
+    cv::imshow("Output", img_f);
+    cv::waitKey(0); // Wait for key press before moving to the next image
 
     // TODO: apply K1 and K2 to img_f with as few multiplications per pixel as possible.
     // Print the number of multiplications your approach uses per pixel.
--- task2/template.cpp
+++ task2/template.cpp
@@ -19,5 +19,9 @@ int main()
     // into a single BGR image, save it as ./assets/memory_restored.png,
     // and print the number of non-zero pixels in the result.
 
+    cv::Mat s1, s2, s3;
+    mat1.convertTo(s1, CV_32S);
+    mat2.convertTo(s2, CV_32S);
+    mat3.convertTo(s3, CV_32S);
     return 0;
 }
